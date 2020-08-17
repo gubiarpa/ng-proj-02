ba869e802b8f7bf52995b30e8e4d406bc82d9181
gubiarpa <billy.arredondo@pucp.pe>
2020-08-17 02:41:11 -0500

    conection service component heroe
    Se estableció la conexión del servicio de Héroes con el componente Heroe (singular).

--- src/app/components/heroe/heroe.component.ts
+++ src/app/components/heroe/heroe.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute }  from '@angular/router';
+import { HeroesService } from '../../services/heroes.service';
 
 @Component({
   selector: 'app-heroe',
@@ -8,7 +10,14 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HeroeComponent implements OnInit {
 
-  constructor() { }
+  heroe: any = {};
+
+  constructor( private _activatedRoute: ActivatedRoute,
+               private _heroesService: HeroesService) { 
+    this._activatedRoute.params.subscribe( params => {
+      this.heroe = this._heroesService.getHeroe( params['id'] );
+    } )
+  }
 
   ngOnInit(): void {
   }
