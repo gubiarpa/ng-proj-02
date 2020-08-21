6af734577d97a48104db1325389b381e685e954f
gubiarpa <billy.arredondo@pucp.pe>
2020-08-21 03:08:38 -0500

    heroe component linked
    Se vinculó la ruta nueva con la ruta y es funcional. Se agregó un mensaje cuando no se encuentra el super héroe.

--- src/app/components/heroes/buscar/buscar.component.ts
+++ src/app/components/heroes/buscar/buscar.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { HeroesService } from 'src/app/services/heroes.service';
 
 @Component({
   selector: 'app-buscar',
@@ -8,9 +10,18 @@ import { Component, OnInit } from '@angular/core';
 })
 export class BuscarComponent implements OnInit {
 
-  constructor() { }
+  heroes: any = {};
+  termino: string;
 
+  constructor( private _activatedRoute: ActivatedRoute,
+               private _heroesService: HeroesService,
+               private _router: Router ) { }
+              
   ngOnInit(): void {
+    this._activatedRoute.params.subscribe( params => {
+      this.termino = params['termino'];
+      this.heroes = this._heroesService.searchHeroe( params['termino'] );
+    });
   }
 
 }
