bc0c6c459c6ebc933b86ab7b30752b65fce854fb
gubiarpa <billy.arredondo@pucp.pe>
2020-08-21 22:30:25 -0500

    heroeTarjeta component updated
    Se modificó el componente heroeTarjeta para que emita el evento con @Output.

--- src/app/components/heroe-tarjeta/heroe-tarjeta.component.ts
+++ src/app/components/heroe-tarjeta/heroe-tarjeta.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
 import { Router } from '@angular/router';
 
 @Component({
@@ -12,13 +12,18 @@ export class HeroeTarjetaComponent implements OnInit {
   @Input() hero: any = {};
   @Input() index: number;
 
-  constructor( private _router: Router ) { }
+  @Output() heroeSeleccionado: EventEmitter<number>;
+
+  constructor( private _router: Router ) {
+    this.heroeSeleccionado = new EventEmitter();
+  }
 
   ngOnInit(): void {
   }
 
   verHeroe() {
-    this._router.navigate(["/heroe", this.index]);
+    // this._router.navigate(["/heroe", this.index]);
+    this.heroeSeleccionado.emit( this.index );
   }
 
 
